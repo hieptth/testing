ce077e2d74ab6682fe43ce49160a8c6f2067c1c2
hieptth <hieptth71@gmail.com>
2023-12-06 08:50:00 +0700

    fix minor problems in readData

--- readData.py
+++ readData.py
@@ -1,13 +1,17 @@
-from xlrd import open_workbook # pip install xlrd ==1.2.0
+from xlrd import open_workbook  # pip install xlrd==1.2.0
+import xlrd
+xlrd.xlsx.ensure_elementtree_imported(False, None)
+xlrd.xlsx.Element_has_iter = True
 
-def readDatafromExcel(sheetName):
+
+def readDataFromExcel(sheetName):
     data = open_workbook("dataTesting.xlsx")
     sheet = data.sheet_by_name(sheetName)
     testcases = []
     for row_index in range(1, sheet.nrows):
         testcase = []
         for col_index in range(sheet.ncols):
-            value = (sheet.cell(row_index,col_index).value)
+            value = sheet.cell(row_index, col_index).value
             try:
                 testcase.append(str(int(value)))
             except:
@@ -16,5 +20,5 @@ def readDatafromExcel(sheetName):
         testcases.append(testcase)
     return testcases
 
-#print(readDatafromExcel('Login'))
-        
+
+print(readDataFromExcel('Login'))
