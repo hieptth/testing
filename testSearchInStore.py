4fafbf3259c2e50a345672c198aed03aa3f6e2d5
hieptth <hieptth71@gmail.com>
2023-12-10 23:43:24 +0700

    add SearchInStore test

--- testSearchInStore.py
+++ testSearchInStore.py
@@ -6,12 +6,12 @@ from readData import readDataFromExcel
 from parameterized import parameterized
 
 
-class SearchTest(unittest.TestCase):
+class SearchInStoreTest(unittest.TestCase):
     def setUp(self) -> None:
         self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(10)
-        self.driver.get("https://www.lazada.vn/")
-        self.driver.implicitly_wait(10)
+        # self.driver.implicitly_wait(10)
+        self.driver.get("https://www.lazada.vn/shop/phong-vu-official-store/")
+        # self.driver.implicitly_wait(10)
         self.driver.set_window_size(1323, 1025)
 
     def tearDown(self) -> None:
@@ -19,19 +19,19 @@ class SearchTest(unittest.TestCase):
 
     def get_result(self, have_result=True):
         if have_result:
-            return self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div[2]/div[1]')
+            return self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div[3]/div[1]/div/div[1]/div[2]/div[1]')
         return self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/div/div[1]/div[2]/div[1]')
 
     def get_input(self):
-        return self.driver.find_element(By.XPATH, '//*[@id="q"]')
+        return self.driver.find_element(By.XPATH, '//*[@id="pi-component-container"]/div/div[2]/div/div/div/div[2]/div[2]/div/div/div[3]/div/input')
 
-    @parameterized.expand(readDataFromExcel("Search"))
-    def test_search(self, have_result, query):
+    @parameterized.expand(readDataFromExcel("SearchInStore"))
+    def test_search_in_store(self, have_result, query):
         input_search = self.get_input()
         input_search.send_keys(query)
         input_search.send_keys(Keys.ENTER)
 
-        self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(20)
         if have_result == "yes":
             result = self.get_result()
             print(result.text, " done")
@@ -41,7 +41,7 @@ class SearchTest(unittest.TestCase):
             self.assertEqual(result.text, "Search No Result")
         else:
             current_url = self.driver.current_url
-            self.assertEqual(current_url, 'https://www.lazada.vn/')
+            self.assertEqual(current_url, 'https://www.lazada.vn/shop/phong-vu-official-store/')
 
 
 if __name__ == "__main__":
